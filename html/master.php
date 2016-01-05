648ffbf4091eac28d46b625cbe50b47206acbc44
rAzzlicious <ze.deputado@gmail.com>
2016-01-05 00:42:20 +0000

    Create master.php

---
+++ html/master.php
@@ -0,0 +1,20 @@
+<html>
+<meta charset="UTF-8">
+<head lang="en">
+    <link href="form1.css" rel="stylesheet">
+
+    <title>
+        <?php echo $titulo; ?>
+    </title>
+
+
+
+</head>
+<!-- INICIO DO BODY -->
+<body>
+    <div>
+        <? php echo $conteudo; ?>
+    </div>
+</body>
+
+</html>
