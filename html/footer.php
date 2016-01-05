ee2c489c55d92c9df8def8e6ecaf188126b02458
rAzzlicious <ze.deputado@gmail.com>
2016-01-05 00:41:52 +0000

    Create footer.php

---
+++ html/footer.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="pt"><head>
+    <title>Inq+</title>
+    <meta name="generator" content="SilverStripe - http://www.silverstripe.com">
+    <meta http-equiv="Content-type" content="text/html; charset=UTF-8">
+    <meta name="keywords" content="ESTGOH , Escola Superior de Tecnologia e Gestão de Oliveira do Hospital , IPC, Instituto Politécnico de Coimbra,  Oliveira do Hospital">
+    <meta name="description" content="Portal da Escola Superior de Tecnologia e Gestão de Oliveira do Hospital">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="gp_ficheiros/img/icon.ico">
+    <link href="gp_ficheiros/css.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="gp_ficheiros/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="gp_ficheiros/layout.css">
+    <link rel="stylesheet" type="text/css" href="gp_ficheiros/typography.css">
+    <link rel="stylesheet" type="text/css" href="gp_ficheiros/layout_002.css">
+    <link rel="stylesheet" type="text/css" href="gp_ficheiros/typography_002.css">
+    <link rel="stylesheet" type="text/css" href="gp_ficheiros/form_002.css">
+</head>
+<body>
+
+    <div class="clear"><!-- --></div>
+
+    <div class="row">
+        <div class="col-md-12" id="Protocols">
+            <a href="http://www.cisco.com/web/PT/learn_events/learn_events_home.html"><img src="gp_ficheiros/cisco.png" alt="Cisco Networking Academy" title="Cisco Networking Academy"></a>
+            <!--<img src="themes/blackcandy/images/protocolos/ms.png" alt="Microsoft Campus Agreement" title="Microsoft Campus Agreement" />
+            <a href="http://www.lpi.org/"><img src="themes/blackcandy/images/protocolos/linux.png" alt="Linux Professional Institute" title="Linux Professional Institute" /></a>-->
+            <img src="gp_ficheiros/spacer.png" alt="imagem usada para separar">
+            <a href="http://www.anet.pt/"><img src="gp_ficheiros/anet.png" alt="Associação Nacional dos Engenheiros Técnicos" title="Associação Nacional dos Engenheiros Técnicos"></a>
+            <a href="http://www.otoc.pt/"><img src="gp_ficheiros/otoc.png" alt="Ordem dos Técnicos Oficiais de Contas" title="Ordem dos Técnicos Oficiais de Contas"></a>
+            <a href="http://www.cmvm.pt/"><img src="gp_ficheiros/cmvm.png" alt="Comissão do Mercado de Valores Mobiliários" title="Comissão do Mercado de Valores Mobiliários"></a>
+            <a href="http://www.act.gov.pt/"><img src="gp_ficheiros/ACT.jpg" alt="Autoridade para as Condições do Trabalho" title="Autoridade para as Condições do Trabalho"></a>
+            <img src="gp_ficheiros/spacer.png" alt="imagem usada para separar">
+            <a href="http://www.b-on.pt/"><img src="gp_ficheiros/bon.png" alt="Biblioteca do Conhecimento Online" title="Biblioteca do Conhecimento Online"></a>
+            <a href="http://websrv2.estgoh.ipc.pt/portal2/#"><img src="gp_ficheiros/eduroam.png" alt="Eduroam" title="Eduroam"></a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12" id="Footer">
+            <div class="footerTop"><!-- --></div>
+            <p>ESTGOH - Escola Superior de Tecnologia e Gestão de Oliveira do Hospital<br>
+                Rua General Santos Costa | 3400-124 Oliveira do Hospital | Portugal<br>
+                Tel: 238 605 170 | Fax: 238 605 179 | Email: geral@estgoh.ipc.pt<br>
+                <a href="http://maps.google.com/maps/ms?hl=pt-PT&amp;ie=UTF8&amp;oe=UTF8&amp;msa=0&amp;msid=107304874900948652387.0004562aaa86b289d780d" style="color:blue" target="blank"> Latitude: 40.360958 | Longitude: - 7.861147</a>
+            </p>
+
+
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12" id="SubFooter">
+            <a href="http://www.acessibilidade.gov.pt/accessmonitor/?cD0yODc2Njk,&amp;wcag20">WCAG 2.0 - AAA</a> |
+            <a href="http://validator.w3.org/check?uri=referer">HTML5</a> |
+            <a href="http://jigsaw.w3.org/css-validator/check/referer">CSS2.1</a> |
+            <a href="http://websrv2.estgoh.ipc.pt/portal2/mapa-do-site">Mapa do Site</a>
+        </div>
+    </div>
+</div>
+
+</body></html>
