61a9d9a409c4c8c253aa90231e51be3d72c10428
wrefer <wrefer1@hotmail.com>
2016-01-05 01:28:38 +0000

    Create BD.php

---
+++ dao/BD.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @access public
+ * @author Diogo
+ */
+class BD {
+	/**
+	 * @AttributeType databasehandler
+	 */
+	private $_dBH;
+
+	/**
+	 * @access public
+	 * @return void
+	 * @ReturnType void
+	 */
+	public function abre_ligacao() {
+		// Not yet implemented
+	}
+
+	/**
+	 * @access public
+	 * @return void
+	 * @ReturnType void
+	 */
+	public function fecha_ligacao() {
+		// Not yet implemented
+	}
+}
+?>
