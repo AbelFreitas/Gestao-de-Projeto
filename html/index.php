141ed8273552e64c66ddf7c53a3eb557a38c415d
daavs <d4vs@hotmail.com>
2016-01-07 04:13:58 +0000

    Update index.php

--- html/index.php
+++ html/index.php
@@ -1,20 +1,23 @@
+<!DOCTYPE html>
 <html>
-<meta charset="UTF-8">
-<head lang="en">
-    <link href="form1.css" rel="stylesheet">
-
-    <title>
-        <?php echo $titulo; ?>
-    </title>
-
-
-
+<head  lang="pt">
+    <title>Inq+</title>
+    <meta name="generator" content="SilverStripe - http://www.silverstripe.com">
+    <meta http-equiv="Content-type" content="text/html; charset=UTF-8">
+    <meta name="keywords" content="ESTGOH , Escola Superior de Tecnologia e Gestão de Oliveira do Hospital , IPC, Instituto Politécnico de Coimbra,  Oliveira do Hospital">
+    <meta name="description" content="Portal da Escola Superior de Tecnologia e Gestão de Oliveira do Hospital">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="gp_ficheiros/img/icon.ico">
+    <link href="gp_ficheiros/css.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="gp_ficheiros/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="gp_ficheiros/layout.css">
+    <link rel="stylesheet" type="text/css" href="gp_ficheiros/typography.css">
+    <link rel="stylesheet" type="text/css" href="gp_ficheiros/layout_002.css">
+    <link rel="stylesheet" type="text/css" href="gp_ficheiros/typography_002.css">
+    <link rel="stylesheet" type="text/css" href="gp_ficheiros/form_002.css">
 </head>
-<!-- INICIO DO BODY -->
-<body>
-    <div>
-        <? php echo $conteudo; ?>
-    </div>
-</body>
+
+
+    <?php echo $conteudo;?>
 
 </html>
