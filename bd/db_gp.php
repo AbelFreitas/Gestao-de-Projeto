1ece5eb85096f925e44b23759ba1df316b416f74
rAzzlicious <ze.deputado@gmail.com>
2016-01-07 06:30:52 +0000

    Update db_gp.php

--- bd/db_gp.php
+++ bd/db_gp.php
@@ -1,1 +1,33 @@
+<?php
 
+class bd {
+    public $DBH = null;
+
+    /**
+     * string abre_ligação(void)
+     *
+     * esta função atribui à variàvel $DBH o link para a base de dados,
+     * retorna o link
+     */
+    public function abre_ligacao(){
+        global $host, $user, $pass, $db;
+
+        try {
+            $this->DBH = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+        } catch(PDOException $e) {
+            echo $e->getMessage();
+            $this->error = $e->getMessage();
+            var_dump($this->error);
+            die("<h2> Erro ao criar link x</h2>");
+        }
+        return $this->DBH;
+    }
+    /**
+     * void fecha_ligacao(void)
+     *
+     *esta função fecha a ligação à base de dados
+     */
+    public function fecha_ligacao(){
+        $this->DBH=null;
+    }
+}
