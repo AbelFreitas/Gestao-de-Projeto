c606f6cc3bbe78c7b582030b643129026489d605
daavs <d4vs@hotmail.com>
2016-01-07 04:16:03 +0000

    Update header.php

--- html/header.php
+++ html/header.php
@@ -1,19 +1,3 @@
-<title>Inq+</title>
-<meta name="generator" content="SilverStripe - http://www.silverstripe.com">
-<meta http-equiv="Content-type" content="text/html; charset=UTF-8">
-<meta name="keywords" content="ESTGOH , Escola Superior de Tecnologia e Gestão de Oliveira do Hospital , IPC, Instituto Politécnico de Coimbra,  Oliveira do Hospital">
-<meta name="description" content="Portal da Escola Superior de Tecnologia e Gestão de Oliveira do Hospital">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="shortcut icon" href="gp_ficheiros/img/icon.ico">
-<link href="gp_ficheiros/css.css" rel="stylesheet" type="text/css">
-<link rel="stylesheet" href="gp_ficheiros/bootstrap.css">
-<link rel="stylesheet" type="text/css" href="gp_ficheiros/layout.css">
-<link rel="stylesheet" type="text/css" href="gp_ficheiros/typography.css">
-<link rel="stylesheet" type="text/css" href="gp_ficheiros/layout_002.css">
-<link rel="stylesheet" type="text/css" href="gp_ficheiros/typography_002.css">
-<link rel="stylesheet" type="text/css" href="gp_ficheiros/form_002.css">
-
-<a href="http://websrv2.estgoh.ipc.pt/portal2/#skipcontent" accesskey="1" style="display:none">Saltar para o corpo principal (tecla de atalho: 1)</a>
 <div class="container">
     <header id="Header" class="bkg_livros">
         <div class="row">
@@ -21,15 +5,13 @@
                 <a href="index.php"><img src="gp_ficheiros/ESTGOH_2015_logo.png" alt="Logotipo ESTGOH" title="logotipo ESTGOH"></a>
             </div>
         </div>
+
         <div class="row">
-            <div class="col-md-12" id="Navigation">
-                <ul>
-                    <li><a href="index.php" title="Ir para Início " class="current">Início</a></li>
-                    <li><a href="index.php" Inq+</a></li>
-                </ul>
-            </div>
         </div>
         <link rel="stylesheet" type="text/css" href="gp_ficheiros/layout_002.css">
         <link rel="stylesheet" type="text/css" href="gp_ficheiros/typography_002.css">
         <link rel="stylesheet" type="text/css" href="gp_ficheiros/form_002.css">
+    </header>
 </div>
+
+
